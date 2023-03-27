3f0c7a3fd7be10139d36801785a47a01481af320
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-03-27 16:48:56 +0200

    Driver class to aid in the DB connection and disconnection. Added method in order to show a timed messagebox when connecting to the db.

--- APP/conexion.py
+++ APP/conexion.py
@@ -9,18 +9,21 @@ import oracledb
 class Conexion:
     def __init__(self):
         oracledb.init_oracle_client()
-        self.config = read_db_config("db_connection_config.ini", 'default')
+        self.config = read_db_config('db_connection_config.ini', 'default')
         try:
+            show_message("Conectando a la BB.DD...", 'info', 1500)
             self.dsn = oracledb.makedsn(self.config['ip'], int(self.config['port']), self.config['service-name'])
             self.conn = oracledb.connect(self.config['user-login'] + self.dsn)
             self.cur = self.conn.cursor()
         except oracledb.DatabaseError:
+            show_message("Imposible conectar a la BB.DD.\nReintentando en 20 segundos...", 'error', 2000)
             self.config = read_db_config("db_connection_config.ini", 'try1')
             try:
                 self.dsn = oracledb.makedsn(self.config['ip'], int(self.config['port']), self.config['service-name'])
                 self.conn = oracledb.connect(self.config['user-login'] + self.dsn)
                 self.cur = self.conn.cursor()
             except oracledb.DatabaseError:
+                show_message("Imposible conectar a la BB.DD.\nReintentando en 20 segundos...", 'error', 2000)
                 self.config = read_db_config("db_connection_config.ini", 'try2')
                 try:
                     self.dsn = oracledb.makedsn(self.config['ip'], int(self.config['port']),
@@ -28,7 +31,9 @@ class Conexion:
                     self.conn = oracledb.connect(self.config['user-login'] + self.dsn)
                     self.cur = self.conn.cursor()
                 except oracledb.DatabaseError:
-                    sys.exit("Imposible conectar a la BB.DD.")
+                    show_message("Imposible conectar a la BB.DD.", 'error', 3000)
+
+                    sys.exit()
 
     def desconectar(self):
         self.conn.close()
@@ -36,6 +41,24 @@ class Conexion:
     def query_fetchall(self, query):
         self.cur.execute(query)
         return self.cur.fetchall()
+
+
+def show_message(message, msg_type='info', timeout=2500):
+    import tkinter as tk
+    from tkinter import messagebox as msgb
+
+    root = tk.Tk()
+    root.withdraw()
+    try:
+        root.after(timeout, root.destroy)
+        if msg_type == 'info':
+            msgb.showinfo('Info', message, master=root)
+        elif msg_type == 'warning':
+            msgb.showwarning('Warning', message, master=root)
+        elif msg_type == 'error':
+            msgb.showerror('Error', message, master=root)
+    except:
+        pass
 
 
 def read_db_config(config_file, section='default'):
@@ -52,6 +75,3 @@ def read_db_config(config_file, section='default'):
         raise Exception(f'Section {section} not found in the {config_file} file')
 
     return db_config
-
-
-
