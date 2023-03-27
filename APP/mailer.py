5e8adc5d3ee63efb85aa7b1230b9f42a2d8c5b56
Gyllenhaal <86900724+GyllenhaalSP@users.noreply.github.com>
2023-03-28 01:39:54 +0200

    Added html object to pass data from factura as tabulated text, also added try catch to print a log file in case mail sending fails.

--- APP/mailer.py
+++ APP/mailer.py
@@ -18,15 +18,15 @@ class Mailer:
         header = "Mail automático enviado por el sistema de facturación de Alimentación Chen S.L.\n\n"
         header += "Se adjunta la factura de la compra realizada en nuestra tienda.\n\n"
         header += "Gracias por su compra.\n\n"
-        header += detalles_factura
+        factura_monospace = f'<pre><font face="Courier New">{detalles_factura}</font></pre>'
         msg = MIMEMultipart()
         msg['Subject'] = f'Alimentación Chen - Factura número {n_fact}'
         msg['From'] = self.remitente
         msg['To'] = destinatario
 
         # Agregar el contenido del correo electrónico
-        text = MIMEText(header)
-        msg.attach(text)
+        msg.attach(MIMEText(header, 'plain'))
+        msg.attach(MIMEText(factura_monospace, 'html'))
 
         # Adjuntar el archivo PDF si se proporcionó
         if adjunto:
@@ -35,8 +35,12 @@ class Mailer:
                 pdf.add_header('content-disposition', 'attachment', filename=os.path.basename(adjunto))
             msg.attach(pdf)
 
-        # Crea la conexión con el servidor de correo electrónico y envía el mensaje
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.login(self.remitente, self.password)
-        server.sendmail(self.remitente, [msg['To']], msg.as_string())
-        server.quit()
+        try:
+            # Crea la conexión con el servidor de correo electrónico y envía el mensaje
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.login(self.remitente, self.password)
+            server.sendmail(self.remitente, [msg['To']], msg.as_string())
+            server.quit()
+        except Exception as e:
+            with open("log.txt", "a") as log_file:
+                log_file.write(f"Error al enviar correo electrónico: {e}\n")
